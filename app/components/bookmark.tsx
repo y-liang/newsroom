f019623be8fe5a4e35552e14e08b61b15b5aa3a4
y-liang <yan@yliang.net>
2023-06-12 22:11:52 -0700

    add hover background

--- app/components/bookmark.tsx
+++ app/components/bookmark.tsx
@@ -14,7 +14,7 @@ export default function Bookmark() {
             type='button'
             disabled={starred.size == 0}
             onClick={() => setOpen(!open)}
-            className='flex items-center gap-2 text-sm text-slate-600 md:enabled:hover:text-slate-800 transition-colors font-normal'>
+            className='flex items-center gap-2 text-sm text-slate-600 md:enabled:hover:text-slate-800 md:enabled:hover:bg-slate-100/75 px-2 py-1 rounded-full transition-colors font-normal'>
             <StarIcon className='h-4 w-4 opacity-50 mb-0.5 sm:mr-2 md:mr-0' />
             <span>{starred.size}</span>
             <span className='hidden md:flex'>{`${starred.size == 1 ? 'Bookmark' : 'Bookmarks'}`}</span>
